703c3cd3418f5d1f79bf24b1968f8962e8482914
Valentin Radulescu <radulescu.valentin@gmail.com>
2025-08-21 19:17:21 +0300

    Clean up old files

--- backend/scripts/tfidf_vector_store.py
+++
@@ -1,7 +0,0 @@
-# loads the TF-IDF matrix data into memory so it can be later accessed inside views
-import sys
-import os
-import numpy as np
-
-filename = os.path.join(os.path.dirname(__file__), '..', 'tfidf_matrix.npy')
-TFIDF_MATRIX = np.load(filename)
