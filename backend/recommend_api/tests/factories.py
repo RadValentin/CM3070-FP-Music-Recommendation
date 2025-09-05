b12dbb5035b6bd5792580298fde45706fc8fa101
Valentin Radulescu <radulescu.valentin@gmail.com>
2025-09-05 19:03:12 +0300

    Fix factories not setting values per instance

--- backend/recommend_api/tests/factories.py
+++ backend/recommend_api/tests/factories.py
@@ -1,38 +1,40 @@
 import factory
 from factory.django import DjangoModelFactory
 from random import randint, choice
-from ..models import Artist, Album, Track
+from recommend_api.models import Artist, Album, Track
 
 
 class ArtistFactory(DjangoModelFactory):
     class Meta:
         model = Artist
-    
-    musicbrainz_artistid = factory.Sequence(lambda n: f'artist-{n}')
-    name = factory.Faker('name')
+
+    musicbrainz_artistid = factory.Sequence(lambda n: f"artist-{n}")
+    name = factory.Faker("name")
 
 
 class AlbumFactory(DjangoModelFactory):
     class Meta:
         model = Album
-    
-    musicbrainz_albumid = factory.Sequence(lambda n: f'album-{n}')
-    name = factory.Faker('sentence', nb_words=2)
-    #artists = factory.SubFactory(ArtistFactory)   
-    date = factory.Faker('date')
+
+    musicbrainz_albumid = factory.Sequence(lambda n: f"album-{n}")
+    name = factory.Faker("sentence", nb_words=2)
+    date = factory.Faker("date")
 
 
 class TrackFactory(DjangoModelFactory):
     class Meta:
         model = Track
-    
-    musicbrainz_recordingid = factory.Sequence(lambda n: f'track-{n}')
+
+    musicbrainz_recordingid = factory.Sequence(lambda n: f"track-{n}")
     album = factory.SubFactory(AlbumFactory)
-    #artists = factory.SubFactory(ArtistFactory)
-    title = factory.Faker('sentence', nb_words=2)
-    duration = randint(1, 1000)
-    genre_dortmund = choice([
-        'electronic','folkcountry','blues','jazz','alternative','rock','raphiphop','pop','funksoulrnb'
-    ])
-    genre_rosamerica = choice(['rhy','dan','pop','roc','cla','hip','jaz','spe'])
-    file_path = factory.Faker('file_path', depth=3, extension='json')
+    title = factory.Faker("sentence", nb_words=2)
+    duration = factory.LazyAttribute(lambda o: randint(1, 1000))
+    genre_dortmund = factory.LazyAttribute(lambda o: choice([
+        "electronic", "folkcountry", "blues", "jazz", "alternative", "rock", "raphiphop", "pop", 
+        "funksoulrnb",
+    ]))
+    genre_rosamerica =  factory.LazyAttribute(lambda o: choice([
+        "rhy", "dan", "pop", "roc", "cla", "hip", "jaz", "spe"
+    ]))
+    submissions = factory.LazyAttribute(lambda o: randint(1, 100))
+    file_path = factory.Faker("file_path", depth=3, extension="json")
