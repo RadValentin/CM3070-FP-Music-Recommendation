326679060cc890127a9a96e0ff4f7ea7c5a9acd8
Valentin Radulescu <radulescu.valentin@gmail.com>
2025-11-12 12:49:36 +0200

    Create FeatureStore class for loading and accessing feature matrix

--- backend/recommend_api/tests/services/test_recommender.py
+++ backend/recommend_api/tests/services/test_recommender.py
@@ -8,25 +8,25 @@ class RecommenderTests(SimpleTestCase):
     ]
 
     def setUp(self):
-        # Override the local variables of the module, removes the need to load feature matrix 
+        # Override the local variables of the module, removes the need to load feature matrix
         # and metadata from disk
 
         # 4 tracks, 3-dim features
-        rec.feature_matrix = np.array([
+        rec.STORE.feature_matrix = np.array([
             [1.0, 0.0, 0.0],  # A
             [0.9, 0.1, 0.0],  # B  (most similar to A)
             [0.2, 1.0, 0.0],  # C
             [0.1, 0.0, 1.0],  # D
         ], dtype=float)
 
-        rec.mbid_to_idx = np.array(['A', 'B', 'C', 'D'])
+        rec.STORE.mbid_to_idx = np.array(['A', 'B', 'C', 'D'])
         # A,B,C in 1990s decade; D in 1980s
-        rec.years = np.array([1991, 1992, 1994, 1983])  
+        rec.STORE.years = np.array([1991, 1992, 1994, 1983])
         # Put A,B,C in same Rosamerica genre, D different
-        rec.genre_rosamerica = np.array(['alt', 'alt', 'alt', 'roc'])
-        rec.genre_dortmund = np.array(['metal', 'jazz', 'metal', 'metal'])
-        rec.feature_names = np.array(['danceability', 'aggressiveness', 'brightness'])
-    
+        rec.STORE.genre_rosamerica = np.array(['alt', 'alt', 'alt', 'roc'])
+        rec.STORE.genre_dortmund = np.array(['metal', 'jazz', 'metal', 'metal'])
+        rec.STORE.feature_names = np.array(['danceability', 'aggressiveness', 'brightness'])
+
     def test_recommend_rosamerica(self):
         out = rec.recommend('A', options={"k":2, "use_ros":True})
 
@@ -45,7 +45,7 @@ class RecommenderTests(SimpleTestCase):
         self.assertEqual(out['stats']['candidate_count'], 1)
         self.assertEqual(out['top_tracks'][0]['mbid'], 'C')
         self.assertEqual(len(out['top_tracks']), 1)
-    
+
     def test_recommend_dortmund(self):
         out = rec.recommend('A', options={"k":2, "use_ros": False})
 
@@ -74,9 +74,9 @@ class RecommenderTests(SimpleTestCase):
 
     def test_feature_stats(self):
         # Make one column near-constant to trigger near_zero_col_count
-        fm = rec.feature_matrix.copy()
-        fm[:, 2] = 0.000001 
-        rec.feature_matrix = fm
+        fm = rec.STORE.feature_matrix.copy()
+        fm[:, 2] = 0.000001
+        rec.STORE.feature_matrix = fm
 
         stats = rec.get_feature_stats()
         assert stats['unique_track_count'] == 4
